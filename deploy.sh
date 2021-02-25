bb4453a37fa93e19f211e4047f1de84220bc58f2
root <root@pxe1.lab.qct.io>
2021-02-25 15:28:07 -0800

    initial working

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-yum install python3
-wget --backups=1 http://10.16.0.1/py-inventory/inventory.py
-wget --backups=1 http://10.16.0.1/py-inventory/requirements.txt
-pip3 install -r requirements.txt
-python3 inventory.py --target http://10.16.0.1/inventories/create
+yum install -y python3
+pip3 install -r /mnt/smbfs/py-inventory/requirements.txt
+python3 /mnt/smbfs/py-inventory/inventory.py --target http://10.16.0.1/inventories/create
